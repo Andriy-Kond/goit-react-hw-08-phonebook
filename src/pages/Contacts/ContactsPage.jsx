4babb3b70aedb47b40a2a4ee2f58d72f36485967
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-05-04 15:12:11 +0300

    contacts fix

--- src/pages/Contacts/ContactsPage.jsx
+++ src/pages/Contacts/ContactsPage.jsx
@@ -1,6 +1,6 @@
 import css from './ContactsPage.module.css';
 import { SmallPreLoader } from 'components/Preloader/PreLoader';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   selectContacts,
   selectError,
@@ -10,11 +10,19 @@ import {
 import { Filter } from 'components/Filter/Filter';
 import { UserForm } from 'components/UserForm/UserForm';
 import { Contacts } from 'components/Contacts/Contacts';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/phonebook/fetchContacts';
 
 const ContactsPage = () => {
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
   const contactsList = useSelector(selectContacts);
+  console.log('ContactsPage >> contactsList:', contactsList);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <>
@@ -23,28 +31,28 @@ const ContactsPage = () => {
           <h2>Phonebook</h2>
           <UserForm />
         </div>
-        {/* {contactsList.length > 0 && (
-          <div> */}
-        <div className={css.contactsItem}>
-          <h2>Contacts</h2>
-          <Filter />
-        </div>
-        <div className={css.contactsItem}>
-          <div className={css.contactsPreloader}>
-            <p>List of contacts</p>
-            <div className={css.preloader}>
-              {isLoading && !error && (
-                <>
-                  <SmallPreLoader /> <SmallPreLoader /> <SmallPreLoader />
-                </>
-              )}
+        {contactsList.length > 0 && (
+          <div>
+            <div className={css.contactsItem}>
+              <h2>Contacts</h2>
+              <Filter />
+            </div>
+            <div className={css.contactsItem}>
+              <div className={css.contactsPreloader}>
+                <p>List of contacts</p>
+                <div className={css.preloader}>
+                  {isLoading && !error && (
+                    <>
+                      <SmallPreLoader /> <SmallPreLoader /> <SmallPreLoader />
+                    </>
+                  )}
+                </div>
+              </div>
+              <Contacts />
+              {error && <h2>Error: {error}</h2>}
             </div>
           </div>
-          <Contacts />
-          {error && <h2>Error: {error}</h2>}
-        </div>
-        {/* </div>
-        )} */}
+        )}
       </div>
     </>
   );
