732f1c72b8c38d1ebe95c632806285170a1a9c20
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-05-04 14:55:03 +0300

    hide contacts if no one contact

--- src/pages/Contacts/ContactsPage.jsx
+++ src/pages/Contacts/ContactsPage.jsx
@@ -2,6 +2,7 @@ import css from './ContactsPage.module.css';
 import { SmallPreLoader } from 'components/Preloader/PreLoader';
 import { useSelector } from 'react-redux';
 import {
+  selectContacts,
   selectError,
   selectIsLoading,
 } from 'redux/phonebook/selectorsPhonebook';
@@ -13,6 +14,7 @@ import { Contacts } from 'components/Contacts/Contacts';
 const ContactsPage = () => {
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+  const contactsList = useSelector(selectContacts);
 
   return (
     <>
@@ -21,24 +23,28 @@ const ContactsPage = () => {
           <h2>Phonebook</h2>
           <UserForm />
         </div>
-        <div className={css.contactsItem}>
-          <h2>Contacts</h2>
-          <Filter />
-        </div>
-        <div className={css.contactsItem}>
-          <div className={css.contactsPreloader}>
-            <p>List of contacts</p>
-            <div className={css.preloader}>
-              {isLoading && !error && (
-                <>
-                  <SmallPreLoader /> <SmallPreLoader /> <SmallPreLoader />
-                </>
-              )}
+        {contactsList.length > 0 && (
+          <div>
+            <div className={css.contactsItem}>
+              <h2>Contacts</h2>
+              <Filter />
+            </div>
+            <div className={css.contactsItem}>
+              <div className={css.contactsPreloader}>
+                <p>List of contacts</p>
+                <div className={css.preloader}>
+                  {isLoading && !error && (
+                    <>
+                      <SmallPreLoader /> <SmallPreLoader /> <SmallPreLoader />
+                    </>
+                  )}
+                </div>
+              </div>
+              <Contacts />
+              {error && <h2>Error: {error}</h2>}
             </div>
           </div>
-          <Contacts />
-          {error && <h2>Error: {error}</h2>}
-        </div>
+        )}
       </div>
     </>
   );
