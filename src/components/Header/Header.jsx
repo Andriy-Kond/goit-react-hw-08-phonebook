0dba0a0c25fe440b3a24b317e69d8b2b5e624eec
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-05-04 14:36:55 +0300

    fix header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,64 @@ import { StyledNavLink } from 'components/StyledNavLink';
 export const Header = () => {
   // const isLoggedIn = useSelector(selectIsLoggedIn);
   const isToken = useSelector(selectIsToken);
+  return (
+    <>
+      <nav
+        className={`navbar navbar-expand-lg bg-body-tertiary ${css.navContainer}`}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="./">
+            LOGO
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul
+              className={`navbar-nav me-auto mb-2 mb-lg-0 ${css.ulContainer}`}
+            >
+              <li className="nav-item">
+                <StyledNavLink to="/">Home</StyledNavLink>
+              </li>
+              <li className="nav-item">
+                {isToken && (
+                  <StyledNavLink to="/contacts">Contacts</StyledNavLink>
+                )}
+              </li>
+
+              <li className={`nav-item ${css.navRight}`}>
+                {isToken ? (
+                  <UserMenu className="nav-link " />
+                ) : (
+                  <ul
+                    className={`navbar-nav me-auto mb-2 mb-lg-0 ${css.ulContainer}`}
+                  >
+                    <li className="nav-item">
+                      <StyledNavLink to="/register">Register</StyledNavLink>
+                    </li>
+                    <li className="nav-item">
+                      <StyledNavLink to="/login">Login</StyledNavLink>
+                    </li>
+                  </ul>
+                )}
+                {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+
   // return (
   //   <>
   //     <nav
@@ -51,60 +109,4 @@ export const Header = () => {
   //     </nav>
   //   </>
   // );
-
-  return (
-    <>
-      <nav
-        className={`navbar navbar-expand-lg bg-body-tertiary ${css.navContainer}`}
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="./">
-            LOGO
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul
-              className={`navbar-nav me-auto mb-2 mb-lg-0 ${css.ulContainer}`}
-            >
-              <li className={`nav-item ${css.navLeft}`}>
-                <StyledNavLink to="/">Home</StyledNavLink>
-              </li>
-
-              <li className={`nav-item ${css.navLeft}`}>
-                {isToken && (
-                  <StyledNavLink to="/contacts">Contacts</StyledNavLink>
-                )}
-              </li>
-
-              <li className={`nav-item ${css.navRight}`}>
-                {isToken ? (
-                  <UserMenu className="nav-link " />
-                ) : (
-                  <ul>
-                    <li className={`nav-item ${css.navLeft}`}>
-                      <StyledNavLink to="/register">Register</StyledNavLink>
-                    </li>
-                    <li className={`nav-item ${css.navLeft}`}>
-                      <StyledNavLink to="/login">Login</StyledNavLink>
-                    </li>
-                  </ul>
-                )}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
 };
